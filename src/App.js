7989460e0c7ec3e7702db2e3f8d000d326fa85ba
Eric  boyer <ericboyer@mbp-de-eric.lan>
2022-04-29 15:16:10 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState([0]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+  
+
+
+      <div className="counter">
+        <button
+          disabled={counter <= 0 && true}
+          onClick={() => {
+            setCounter(counter - 1);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          -
+        </button>
+
+        <p className="zero">{counter}</p>
+        <button
+          disabled={counter >= 10 && true}
+          onClick={() => {
+            setCounter(counter + 1);
+          }}
+        >
+          +
+        </button >
+      </div>
+      <button
+        onClick={() => {
+          setCounter(0);
+        }}
+     className="reset" >
+        RESET
+      </button>
+
+<button onClick={()=>{
+const newCounter = [...counter];
+
+newCounter.push({useState})
+
+setCounter(newCounter);
+console.log(newCounter)
+}}>Add counter</button>
+
     </div>
   );
 }
